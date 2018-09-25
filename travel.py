0c13638fda40cdb575c73e426a6e6211bb06e3aa
chenj <junfengchen2016@sina.com>
2018-09-25 14:35:30 +0800

    1

--- travel.py
+++ travel.py
@@ -10,6 +10,7 @@ gflags.DEFINE_string('path_txt', './travel/txt', '')
 gflags.DEFINE_string('path_txt2', './travel/txt2', '')
 gflags.DEFINE_string('path_mp3', './travel/mp3', '')
 gflags.DEFINE_string('pathname_md', './travel/tianjin.md', '')
+gflags.DEFINE_string('md_h1', '天津', '')
 gflags.DEFINE_string('pathname_src', '', '')
 gflags.DEFINE_string('pathname_dst', '', '')
 FLAGS = gflags.FLAGS
@@ -57,8 +58,9 @@ def batch_txt2mp3(path_txt, path_mp3):
 
         txt2mp3(pathname_txt, pathname_mp3)  
 
-def batch_txt2md(path_txt, pathname_md):
+def batch_txt2md(h1, path_txt, pathname_md):
     filenames_txt = os.listdir(path_txt)
+    dict1 = {}
     items = []
     for i, filename_txt in enumerate(filenames_txt):
         stem, extension = os.path.splitext(filename_txt)
@@ -68,12 +70,27 @@ def batch_txt2md(path_txt, pathname_md):
         with open(pathname_txt, 'r', encoding='utf-8') as f:
             lines = [line.strip() for line in f.readlines()]
             tex = ''.join(lines)
-            items.append({'h2': stem, 'h3':tex})
+            b = [a.strip() for a in stem.split('-')]
+            if(len(b) == 2):
+                key = b[0]
+                value = dict1.get(key, [])
+                value.append({'title': b[1], 'content':tex})
+                dict1[key] = value
+            else:
+                key = b['others']
+                value = dict1.get(key, [])
+                value.append({'title': stem, 'content':tex})
+                dict1[key] = value
+            items.append({'title': stem, 'content':tex})
 
     with open(pathname_md, 'w', encoding='utf-8') as f:
-        for item in items:
-            f.write('- {}\n'.format(item['h2']))
-            f.write('```\n{}\n```\n'.format(item['h3']))
+        f.write('# {}\n'.format(h1))
+        for key in dict1.keys():
+            f.write('## {}\n'.format(key))
+            for item in dict1[key]:
+        # for item in items:
+                f.write('- {}\n'.format(item['title']))
+                f.write('```\n{}\n```\n'.format(item['content']))
 
 def main(argv):
     FLAGS(argv)
@@ -82,6 +99,7 @@ def main(argv):
     path_txt2 = FLAGS.path_txt2
     path_mp3 = FLAGS.path_mp3
     pathname_md = FLAGS.pathname_md
+    md_h1 = FLAGS.md_h1
     pathname_src = FLAGS.pathname_src
     pathname_dst = FLAGS.pathname_dst
 
@@ -90,7 +108,7 @@ def main(argv):
     if(path_txt2 != '' and path_mp3 != ''):        
         batch_txt2mp3(path_txt2, path_mp3)
     if(path_txt2 != '' and pathname_md != ''):                
-        batch_txt2md(path_txt2, pathname_md)
+        batch_txt2md(md_h1, path_txt2, pathname_md)
 
 if __name__ == '__main__':
     sys.exit(int(main(sys.argv) or 0))
